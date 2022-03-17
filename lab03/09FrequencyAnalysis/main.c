caedebec905fb1ca8d9cb8dfab231934c99fc579
Semen Yurkov <semen.yurkov@unli.lu>
2022-03-17 08:42:54 +0100

    lab03

---
+++ lab03/09FrequencyAnalysis/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<ctype.h>
+
+#define ALPHABET_SIZE 26
+
+int main() {
+
+	int letters[ALPHABET_SIZE], c;
+
+	// initialization of the frequency table
+	for(int i = 0; i < ALPHABET_SIZE; )
+		letters[i++] = 0;
+
+	// input read in
+	while((c = getchar()) != EOF) {
+		// case-insensitive
+		c = toupper(c);
+
+		if(c >= 'A' && c <= 'Z')
+			letters[c - 'A']++;
+	}
+
+	// print frequency table
+	for(int i = 0; i < ALPHABET_SIZE; ++i)
+		printf("%c: %5d\n", 'A' + i, letters[i]);
+
+	return 0;
+}
---
+++ lab03/10MergingSortedArrays/array.h
@@ -0,0 +1,8 @@
+#ifndef ARRAY_H_
+#define ARRAY_H_
+
+void fill_array(int array[], int size);
+void sort_array(int array[], int size);
+void print_array(int array[], int size);
+
+#endif /* ARRAY_H_ */
